f9292fd2a9e00acbf47084b3fa1ad0dc61c572e3
lx <2571675h@student.gla.ac.uk>
2021-08-04 10:47:41 +0800

    无

---
+++ 315.计算右侧小于当前元素的个数/315. 计算右侧小于当前元素的个数.go
@@ -0,0 +1,6 @@
+package leetcode
+
+func countSmaller(nums []int) []int {
+	res := make([]int, len(nums))
+	return res
+}
---
+++ 773.滑动谜题/773. 滑动谜题.go
@@ -0,0 +1,30 @@
+package leetcode
+
+// int steps
+// end:=mars([[1,2,3],[4,5,0]])
+// dirs:=[[1,0]]
+
+// func slidingPuzzle(board [][]int) int {
+// 	steps=-1
+// 	sta:=mars(board)
+// 	bfs([sta])
+// 	return steps
+// }
+
+// func bfs(queue []string) next_queue []string{
+// 	for node:= range queue{
+// 		if node==end{
+// 			steps+=1
+// 			break
+// 		}
+
+// 		add(next_queue,nextstep)
+// 	}
+// 	if steps!=-1{
+// 		steps+=1
+// 	}
+// }
+
+// func mars(input [][]int) output string{
+
+// }
